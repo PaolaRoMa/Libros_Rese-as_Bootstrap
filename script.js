1842096bb5b1422fdc19ac8510eec8d100829203
PaolaRoMa <miriam.rodriguez4335@alumnos.udg.mx>
2024-02-02 16:51:29 -0600

    Cambio de cartas a paginas diferentes

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 // script.js
-document.addEventListener('DOMContentLoaded', function () {
+/*document.addEventListener('DOMContentLoaded', function () {
   const ratings = document.querySelectorAll('.rating');
 
   ratings.forEach(rating => {
       const bookId = rating.dataset.bookId;
 
       rating.addEventListener('click', function (event) {
-          const selectedRating = event.target.previousElementSibling.value;
+          const selectedRating = event.target.AnteriorElementSibling.value;
 
           console.log(`Libro ID ${bookId}: Calificación seleccionada - ${selectedRating}`);
           // Puedes agregar lógica para enviar la calificación al servidor u otras acciones
       });
   });
 });
+*/
