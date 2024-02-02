f47b88810f889160282691e86e713e803068bdcf
PaolaRoMa <miriam.rodriguez4335@alumnos.udg.mx>
2024-02-01 21:23:51 -0600

    Primer commit

---
+++ script.js
@@ -0,0 +1,15 @@
+// script.js
+document.addEventListener('DOMContentLoaded', function () {
+  const ratings = document.querySelectorAll('.rating');
+
+  ratings.forEach(rating => {
+      const bookId = rating.dataset.bookId;
+
+      rating.addEventListener('click', function (event) {
+          const selectedRating = event.target.previousElementSibling.value;
+
+          console.log(`Libro ID ${bookId}: Calificación seleccionada - ${selectedRating}`);
+          // Puedes agregar lógica para enviar la calificación al servidor u otras acciones
+      });
+  });
+});
